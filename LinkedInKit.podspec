a79dd4ee08cef1fd18a1a4399d2448aaedd08352
Mariana <mariana.ristovska@webfactory.mk>
2016-07-08 14:28:39 +0200

    LinkedIn idk added to pod

--- LinkedInKit.podspec
+++ LinkedInKit.podspec
@@ -1,21 +1,7 @@
-#
-# Be sure to run `pod lib lint LinkedInKit.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = 'LinkedInKit'
   s.version          = '0.1.0'
   s.summary          = 'A short description of LinkedInKit.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
@@ -25,18 +11,16 @@ TODO: Add long description of the pod here.
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Mariana' => 'mariana.ristovska@webfactory.mk' }
   s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/LinkedInKit.git', :tag => s.version.to_s }
-
+  s.requires_arc = true
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'LinkedInKit/Classes/**/*'
-  
-  # s.resource_bundles = {
-  #   'LinkedInKit' => ['LinkedInKit/Assets/*.png']
-  # }
+s.preserve_paths      = 'linkedin-sdk.framework'
+s.vendored_frameworks = 'linkedin-sdk.framework'
+s.source_files =  ['LinkedInKit/Classes/**/*', 'linkedin-sdk.framework/Headers/LISDKAccessToken.h', 'linkedin-sdk.framework/Headers/LISDKAPIError.h', 'linkedin-sdk.framework/Headers/LISDKAPIHelper.h', 'linkedin-sdk.framework/Headers/LISDKAPIResponse.h', 'linkedin-sdk.framework/Headers/LISDKCallbackHandler.h', 'linkedin-sdk.framework/Headers/LISDKDeeplinkHelper.h', 'linkedin-sdk.framework/Headers/LISDKErrorCode.h', 'linkedin-sdk.framework/Headers/LISDKPermission.h', 'linkedin-sdk.framework/Headers/LISDKSession.h', 'linkedin-sdk.framework/Headers/LISDKSessionManager.h']
+# s.public_header_files = 'linkedin-sdk.framework/Headers/*.h'
 
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit'
-  
+s.xcconfig = { "HEADER_SEARCH_PATHS" => "./linkedin-sdk.framework/Headers/LISDK.h" }
+
   s.dependency 'Alamofire'
-  
+
 end
---
+++ LinkedInKit/Classes/LinkedInAuthenticator.swift
@@ -0,0 +1,124 @@
+import Foundation
+
+typealias LinkedInAuthSuccessCallback = (token: LinkedInAccessToken?) -> ()
+typealias LinkedInAuthFailureCallback = (error: NSError?) -> ()
+typealias LinkedInRequestSuccessCallback = (response: LinkedInSDKResponse?) -> ()
+typealias LinkedInRequestFailureCallback = (error: LISDKAPIError?) -> ()
+
+class LinkedInAuthenticator: NSObject {
+    
+    private let configuration: LinkedInConfiguration
+    private let httpClient: LinkedInHTTPClient
+    
+    var accessToken: LinkedInAccessToken?
+    var hasValidAccessToken: Bool {
+        return accessToken != nil && accessToken?.expireDate > NSDate()
+    }
+    
+    init(configuration: LinkedInConfiguration, httpClient: LinkedInHTTPClient) {
+        self.configuration = configuration
+        self.httpClient = httpClient
+        super.init()
+    }
+    
+    // MARK: Authentication
+    func authenticate(success: LinkedInAuthSuccessCallback?,
+                      failure: LinkedInAuthFailureCallback?) {
+        
+        // Check if previous token is still in memory and is valid
+        if let accessToken = accessToken where accessToken.expireDate > NSDate() {
+            success?(token: accessToken)
+        } else {
+            
+            // Use LinkedInSDK if app is installed
+            if LinkedInAuthenticator.isLinkedInAppInstalled {
+                
+                let session = LISDKSessionManager.sharedInstance().session
+                accessToken = LinkedInAuthenticator.tokenFromSDKSession(session)
+                
+                if session.isValid() && hasValidAccessToken {
+                    success?(token: accessToken)
+                }  else {
+                    //Authorize through SDK
+                    LISDKSessionManager.createSessionWithAuth(configuration.permissions,
+                                                              state: configuration.state,
+                                                              showGoToAppStoreDialog: false,
+                                                              successBlock:
+                        { [weak self] (response) in
+                            self?.accessToken = LinkedInAuthenticator.tokenFromSDKSession(LISDKSessionManager.sharedInstance().session)
+                            success?(token: self?.accessToken)
+                        }, errorBlock: { (error) in
+                            failure?(error: error)
+                    })
+                }
+            } else {
+                httpClient.getAuthorizationCode(withSuccessCalback: { [weak self] (code) in
+                    self?.httpClient.getAccessToken(forAuthorizationCode: code, success: { [weak self] (token) in
+                        self?.accessToken = token
+                        success?(token: self?.accessToken)
+                        }, failure: { (error) in
+                            failure?(error: error)
+                    })
+                    }, cancelCallback: {
+                        //TODO: Send appropirate error
+                    }, failureCallback: { (error) in
+                        failure?(error: error)
+                })
+            }
+        }
+    }
+    
+    private static func tokenFromSDKSession(session: LISDKSession) -> LinkedInAccessToken {
+        let session = LISDKSessionManager.sharedInstance().session
+        return LinkedInAccessToken(withAccessToken: session.accessToken.accessTokenValue,
+                                   expireDate: session.accessToken.expiration,
+                                   isSDK: true)
+    }
+    
+    //MARK: - Requests 
+    func requestUrl(url: NSURL, success: LinkedInRequestSuccessCallback?, failure: LinkedInRequestFailureCallback?) {
+        
+        // **NOTE** Only GET request 
+        
+        if hasValidAccessToken {
+            if LinkedInAuthenticator.isLinkedInAppInstalled {
+                LISDKAPIHelper.sharedInstance().getRequest("",
+                                                           success:
+                    { (response) in
+                        
+                        if let dataFromString = response.data.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+                            success?(response: LinkedInSDKResponse(withData: dataFromString, statusCode: Int(response.statusCode)))
+                        } else {
+                            success?(response: nil)
+                        }
+                        
+                    }, error: { (error) in
+                        failure?(error: error)
+                })
+            }
+        }
+    }
+}
+
+//MARK: - Static methods
+extension LinkedInAuthenticator {
+    
+    static var isLinkedInAppInstalled: Bool {
+        return UIApplication.sharedApplication().canOpenURL(NSURL(string: "linkedin://")!)
+    }
+    
+    static func shouldHandleUrl(url: NSURL) -> Bool {
+        return isLinkedInAppInstalled && LISDKCallbackHandler.shouldHandleUrl(url)
+    }
+    
+    static func application(application: UIApplication,
+                            openURL url: NSURL,
+                            sourceApplication: String,
+                            annotation: AnyObject) -> Bool {
+        
+        return isLinkedInAppInstalled && LISDKCallbackHandler.application(application,
+                                                                            openURL: url,
+                                                                            sourceApplication: sourceApplication,
+                                                                            annotation: annotation)
+    }
+}
